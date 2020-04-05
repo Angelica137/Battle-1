d94e4971bfc681946d7c0ec59c1392be94d889a0
Angelica <Angelica137@users.noreply.github.com>
2020-04-05 19:17:00 +0100

    see oponent's HP green

--- Makers/wk_3/Battle/Battle-1/spec/features/hit_points_spec.rb
+++ Makers/wk_3/Battle/Battle-1/spec/features/hit_points_spec.rb
@@ -1,6 +1,9 @@
 feature 'See oponents points' do
   scenario 'Player 1 sees Player 2 HP' do
-    visit('/play')
-    expect(page).to have_content 'Mittens HP: 100'
+    visit('/')
+    fill_in :player_1_name, with: 'Dave'
+    fill_in :player_2_name, with: 'Mittens'
+    click_button 'Submit'
+    expect(page).to have_content 'Mittens: 100 HP'
   end
 end
