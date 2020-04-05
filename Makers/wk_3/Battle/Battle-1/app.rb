af849153b6ac1b665450ac6a660ee47ec0813847
Angelica <Angelica137@users.noreply.github.com>
2020-04-05 16:19:19 +0100

    prepare capybara to test app

--- Makers/wk_3/Battle/Battle-1/app.rb
+++ Makers/wk_3/Battle/Battle-1/app.rb
@@ -3,6 +3,8 @@ require 'sinatra/base'
 class Battle < Sinatra::Base
   get '/' do
     'Hello Battle!'
+    #on landing the users should be able
+    #to input their names
   end
 
   run! if app_file == $0
