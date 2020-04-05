0c3a489ba39dc6eb7ca7d6de56217ae2abb69273
Angelica <Angelica137@users.noreply.github.com>
2020-04-05 20:01:34 +0100

    Player 1 attacks Player 2 test, red

--- Makers/wk_3/Battle/Battle-1/spec/features/hit_points_spec.rb
+++ Makers/wk_3/Battle/Battle-1/spec/features/hit_points_spec.rb
@@ -4,3 +4,11 @@ feature 'See oponents points' do
     expect(page).to have_content 'Mittens: 100 HP'
   end
 end
+
+feature 'Attack oponent' do
+  scenario 'Player 1 attacks Player 2 and sees confirmation' do
+    sign_in_and_play
+    click_button 'Attack'
+    expect(page).to have_content 'You attacked Mittens'
+  end
+end
