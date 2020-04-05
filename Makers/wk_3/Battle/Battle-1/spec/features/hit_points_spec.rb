9dabf16194ef62f4633c74b64206bd34465a4afe
Angelica <Angelica137@users.noreply.github.com>
2020-04-05 19:53:53 +0100

    refactor tests with helper

--- Makers/wk_3/Battle/Battle-1/spec/features/hit_points_spec.rb
+++ Makers/wk_3/Battle/Battle-1/spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'See oponents points' do
   scenario 'Player 1 sees Player 2 HP' do
-    visit('/')
-    fill_in :player_1_name, with: 'Dave'
-    fill_in :player_2_name, with: 'Mittens'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Mittens: 100 HP'
   end
 end
