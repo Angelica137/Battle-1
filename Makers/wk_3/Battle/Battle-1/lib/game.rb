fab4a4ca58d08187d364c532c4700832c52b4931
Angelica <Angelica137@users.noreply.github.com>
2020-04-06 20:16:34 +0100

    add turn attribute, test green

--- Makers/wk_3/Battle/Battle-1/lib/game.rb
+++ Makers/wk_3/Battle/Battle-1/lib/game.rb
@@ -1,14 +1,16 @@
 require_relative 'player.rb'
 
 class Game
-  attr_reader :player_1, :player_2
+  attr_reader :player_1, :player_2, :turn
 
   def initialize(player_1, player_2)
     @player_1 = player_1
     @player_2 = player_2
+    @turn = player_1
   end
 
   def attack(player)
     player.receive_attack
   end
+
 end
