f422a191ae3622c5e48f8108737d56f71cc20648
Angelica <Angelica137@users.noreply.github.com>
2020-04-06 20:00:56 +0100

    add switch turn scenario, red test

--- Makers/wk_3/Battle/Battle-1/spec/features/switch_turns_spec.rb
+++ Makers/wk_3/Battle/Battle-1/spec/features/switch_turns_spec.rb
@@ -3,4 +3,13 @@ feature 'current_turn' do
     sign_in_and_play
       expect(page).to have_content 'Dave, attack!'
   end
+
+  scenario 'after Player_1 attacks, Player_2 goes' do
+    sign_in_and_play
+    click_button 'Attack'
+    click_link 'OK'
+    expect(page).not_to have_content "Dave, attack!"
+    expect(page).to have_content "Mittens, go!"
+  end
+
 end
