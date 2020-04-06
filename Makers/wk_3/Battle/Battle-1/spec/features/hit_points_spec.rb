13d0149e1b34ee40cf2143046812cc4dc31217ab
Angelica <Angelica137@users.noreply.github.com>
2020-04-06 17:00:15 +0100

    remove obsolete HP test

--- Makers/wk_3/Battle/Battle-1/spec/features/hit_points_spec.rb
+++ Makers/wk_3/Battle/Battle-1/spec/features/hit_points_spec.rb
@@ -1,9 +1,3 @@
-feature 'See oponents points' do
-  scenario 'Player 1 sees Player 2 HP' do
-    sign_in_and_play
-    expect(page).to have_content 'Mittens: 100 HP'
-  end
-end
 
 feature 'Attack oponent' do
   scenario 'Player 1 attacks Player 2 and sees confirmation' do
