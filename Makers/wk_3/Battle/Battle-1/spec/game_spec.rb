132d6f3a76e730200a70448c1fa91f39aa6c3619
Angelica <Angelica137@users.noreply.github.com>
2020-04-06 17:56:46 +0100

    refactor game test

--- Makers/wk_3/Battle/Battle-1/spec/game_spec.rb
+++ Makers/wk_3/Battle/Battle-1/spec/game_spec.rb
@@ -1,13 +1,13 @@
 require 'game'
 
 describe Game do
+  subject(:game) { described_class.new }
+  let(:dave) { double :player }
+  let(:mittens) { double :player }
   describe '#attack' do
   it 'damages the oponent' do
-    game = Game.new
-    dave = Player.new('Dave')
-    mittens = Player.new('Mittens')
     expect(mittens).to receive(:receive_attack)
-    dave.attack(mittens)
+    game.attack(mittens)
   end
 end
   
